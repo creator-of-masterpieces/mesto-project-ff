48c98f809ed0fc38c7e784ca898edea18d98fd32
Dmitrii Timonov <42276523+creator-of-masterpieces@users.noreply.github.com>
2025-04-27 21:49:34 +0300

    Добавил функции создания карточек и добавления их при загрузке страницы

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,54 @@
-// @todo: Темплейт карточки
+// Записал в переменную $ вызов метода querySelector, привязанного к объекту document
+const $ = document.querySelector.bind(document);
 
-// @todo: DOM узлы
+// Записал список карточек в переменную placesList
+const placesList = $('.places__list');
 
-// @todo: Функция создания карточки
+// Сохраняю шаблон карточки
+const cardTemplate = $('#card-template');
+
+//Функция создания карточки
+function createCard(cardData) {
+
+// Копирую содержимое шаблона карточки
+    const cardItem = cardTemplate.content.cloneNode(true);
+
+
+// Сохраняю кнопку удаления карточки
+    const deleteButton = cardItem.querySelector('.card__delete-button');
+
+
+// Добавляю обработчик клика на кнопку удаления карточки
+    deleteButton.addEventListener('click', () => {
+        deleteCard(cardItem);
+    });
+
+
+
+// Задаю текст заголовка карточки
+    cardItem.querySelector('.card__title').textContent = cardData.name;
+
+// Задаю значение атрибутов картинки
+    Object.assign(cardItem.querySelector('.card__image'), {
+        src: cardData.link,
+        alt: cardData.name,
+    })
+
+// Ворзвращаю заполненную карточку
+    return cardItem;
+}
+
+//Функция удаления карточек
+function deleteCard(element) {
+    element.closest('.card').remove();
+}
+
+// Передаю элементы массива с информацией о карточках в функцию создания карточки
+initialCards.forEach((item) => {
+
+// Добавляю на страницу список заполненных карточек
+    placesList.append(createCard(item));
+})
+
 
 // @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
