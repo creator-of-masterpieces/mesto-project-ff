8087497409169b51ba8753568753e00464c7218d
Dmitrii Timonov <42276523+creator-of-masterpieces@users.noreply.github.com>
2025-04-29 00:37:12 +0300

    Изменил функцию создания карточки. Прочие мелкие исправления
    Добавил в функцию createCard параметр для передачи функции удаления карточки.
    Сохранил обложку карточки в переменную

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,34 @@
 // Записал в переменную $ вызов метода querySelector, привязанного к объекту document
 const $ = document.querySelector.bind(document);
 
-// Записал список карточек в переменную placesCardList
-const placesCardList = $('.places__list');
-
 // Сохраняю шаблон карточки
 const cardTemplate = $('#card-template').content;
 
-//Функция создания карточки
-function createCard(cardData) {
+// Записал список карточек в переменную placesCardList
+const placesCardList = $('.places__list');
+
+//Функция создания карточки принимает данные для заполнения карточки и функцию удаления карточки
+function createCard(cardData, deleteFunction) {
 
 // Копирую содержимое шаблона карточки
     const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
 
-
 // Сохраняю кнопку удаления карточки
     const deleteButton = cardItem.querySelector('.card__delete-button');
 
+// Сохраняю обложку карточки
+    const cardImage = cardItem.querySelector('.card__image');
 
 // Добавляю обработчик клика на кнопку удаления карточки
     deleteButton.addEventListener('click', () => {
-        deleteCard(cardItem);
+        deleteFunction(cardItem);
     });
-
-
 
 // Задаю текст заголовка карточки
     cardItem.querySelector('.card__title').textContent = cardData.name;
 
 // Задаю значение атрибутов картинки
-    Object.assign(cardItem.querySelector('.card__image'), {
+    Object.assign(cardImage, {
         src: cardData.link,
         alt: cardData.name,
     })
@@ -47,5 +46,5 @@ function deleteCard(element) {
 initialCards.forEach((item) => {
 
 // Добавляю на страницу список заполненных карточек
-placesCardList.append(createCard(item));
+placesCardList.append(createCard(item, deleteCard));
 })
