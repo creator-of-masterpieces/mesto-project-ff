3ff6d642c154c1cc03269d5e6d2bd6a1aac3a7fd
Dmitrii Timonov <42276523+creator-of-masterpieces@users.noreply.github.com>
2025-06-21 19:14:47 +0300

    Добавил функции: Функция-инициализатор для инициализации валидации при загрузке страницы; Функция для показа ошибок; Функция для скрытия ошибок; Функция для проверки валидности полей; Функция для переключения активной\неактивной кнопки; Функция для установки слушателей на полу формы; Функция для очистки контейнеров с ошибками;

---
+++ src/components/validation.js
@@ -0,0 +1,75 @@
+export function showInputError(formElement, inputElement, errorMessage, config) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add(config.inputErrorClass);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(config.errorClass);
+}
+
+export function hideInputError(formElement, inputElement, config) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove(config.inputErrorClass);
+    errorElement.textContent = '';
+    errorElement.classList.remove(config.errorClass);
+}
+
+function checkInputValidity(formElement, inputElement, config) {
+    if (inputElement.validity.patternMismatch) {
+        inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+    } else {
+        inputElement.setCustomValidity('');
+    }
+
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, inputElement.validationMessage, config);
+    } else {
+        hideInputError(formElement, inputElement, config);
+    }
+}
+
+function hasInvalidInput(inputList) {
+    return inputList.some((inputElement) => !inputElement.validity.valid);
+}
+
+function toggleButtonState(inputList, buttonElement, config) {
+    if (hasInvalidInput(inputList)) {
+        buttonElement.classList.add(config.inactiveButtonClass);
+        buttonElement.setAttribute('disabled', true);
+    } else {
+        buttonElement.classList.remove(config.inactiveButtonClass);
+        buttonElement.removeAttribute('disabled');
+    }
+}
+
+function setEventListeners(formElement, config) {
+    const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+    const buttonElement = formElement.querySelector(config.submitButtonSelector);
+    toggleButtonState(inputList, buttonElement, config);
+
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener('input', () => {
+            checkInputValidity(formElement, inputElement, config);
+            toggleButtonState(inputList, buttonElement, config);
+        });
+    });
+}
+
+export function enableValidation(config) {
+    const formList = Array.from(document.querySelectorAll(config.formSelector));
+    formList.forEach((formElement) => {
+        formElement.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+        });
+        setEventListeners(formElement, config);
+    });
+}
+
+export function clearValidation(formElement, config) {
+    const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+    const buttonElement = formElement.querySelector(config.submitButtonSelector);
+
+    inputList.forEach((inputElement) => {
+        hideInputError(formElement, inputElement, config);
+    });
+
+    toggleButtonState(inputList, buttonElement, config);
+}
